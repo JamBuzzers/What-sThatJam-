c85a971ed5b3cdb6a3679f0405b9e9a0480b2d02
Latifat Ozoya <latifat@fb.com>
2018-07-24 16:38:54 -0700

    Fragments and bottom navigation bar

--- app/src/main/java/com/jambuzzers/whatsthatjam/SearchableFragment.java
+++ app/src/main/java/com/jambuzzers/whatsthatjam/SearchableFragment.java
@@ -11,7 +11,7 @@ public class SearchableFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_browse, container, false);
+        return inflater.inflate(R.layout.fragment_searchable, container, false);
     }
 
 }
