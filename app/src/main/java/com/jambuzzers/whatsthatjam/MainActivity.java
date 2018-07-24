b1ad254bf41e6841a57cc49e4d941bb8ec020903
Callum Pe <callumpe@fb.com>
2018-07-23 19:42:58 -0700

    added butterknife and the very beginning of the Main Activity

--- app/src/main/java/com/jambuzzers/whatsthatjam/MainActivity.java
+++ app/src/main/java/com/jambuzzers/whatsthatjam/MainActivity.java
@@ -1,13 +1,21 @@
 package com.jambuzzers.whatsthatjam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+   @BindView(R.id.placeholder)
+    FrameLayout placeholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
