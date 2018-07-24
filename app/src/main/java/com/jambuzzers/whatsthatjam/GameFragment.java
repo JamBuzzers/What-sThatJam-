575502e56c032f3dd57f43b9a1ea085aaacd157d
Callum Pe <callumpe@fb.com>
2018-07-24 16:53:56 -0700

    added lessley's game fragment

---
+++ app/src/main/java/com/jambuzzers/whatsthatjam/GameFragment.java
@@ -0,0 +1,68 @@
+package com.jambuzzers.whatsthatjam;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Handles all the game view. Checks the edit text and gets a track from the array 20 to play
+ * Should handle when the song plays and stops the song
+ */
+public class GameFragment extends Fragment {
+
+
+    @BindView(R.id.guess_btn) Button stopBtn;
+    @BindView(R.id.etGuess) EditText etSongGuess;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_game, container, false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initGuessAccess();
+                //TODO: Stop music after designated time...call function that handles that...
+            }
+        });
+        etSongGuess.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                boolean handled = false;
+                if(i == EditorInfo.IME_ACTION_DONE){}
+                    //TODO send to response to server
+
+                return handled;
+            }
+        });
+
+    }
+
+    public void initGuessAccess() {
+        stopBtn.setEnabled(false);
+        etSongGuess.setEnabled(true);
+    }
+
+
+}
