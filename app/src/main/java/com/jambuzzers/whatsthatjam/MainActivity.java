5dc92a4d104c8687262b66a56841c766fb87085d
Lessley Hernandez <lessley@fb.com>
2018-07-31 14:09:56 -0700

    Skeleton of viewPager fix and player in game frag

--- app/src/main/java/com/jambuzzers/whatsthatjam/MainActivity.java
+++ app/src/main/java/com/jambuzzers/whatsthatjam/MainActivity.java
@@ -5,6 +5,8 @@ package com.jambuzzers.whatsthatjam;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -12,12 +14,13 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
-import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
+import com.jambuzzers.whatsthatjam.model.SocketPlayer;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
-import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.Spotify;
 
 
@@ -25,7 +28,40 @@ public class MainActivity extends AppCompatActivity{
 
     final FragmentManager fragmentManager = getSupportFragmentManager();
     final Fragment loginFrag = new LoginFragment();
-    private Player mPlayer;
+
+
+    final Fragment gFrag = new GameFragment();
+    final Fragment pFrag = new ProfileFragment();
+    final Fragment sFrag = new SearchableFragment();
+
+
+
+    GameFragment gameFragment;
+    SocketPlayer player;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener=
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.search:
+                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                            fragmentTransaction.replace(R.id.fragment , sFrag).commit();
+                            return true;
+                        case R.id.play:
+                            FragmentTransaction fragmentTransaction2 = fragmentManager.beginTransaction();
+                            fragmentTransaction2.replace(R.id.fragment, gFrag).commit();
+                            return true;
+                        case R.id.profile:
+                            FragmentTransaction fragmentTransaction3 = fragmentManager.beginTransaction();
+                            fragmentTransaction3.replace(R.id.fragment, pFrag).commit();
+                            return true;
+                    }
+                    return false;
+                }
+            };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +69,15 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment, loginFrag).commit();
+
 
         ViewPager pager = findViewById(R.id.view_pager);
         pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
@@ -45,6 +88,46 @@ public class MainActivity extends AppCompatActivity{
         super.onActivityResult(requestCode, resultCode, intent);
         if (requestCode == LoginFragment.REQUEST_CODE) {
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            player = new SocketPlayer(response, this, new SocketPlayer.SocketPlayerListener() {
+                @Override
+                public void onResume() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Resumed",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                @Override
+                public void onPlay() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Played",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                @Override
+                public void onPause() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Pause",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                @Override
+                public void onInvite(int gameId) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Invited ",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    player.acceptGame(gameId);
+                }
+            }) ;
+            gameFragment.setListener(player);
         }
 
     }
@@ -68,24 +151,22 @@ public class MainActivity extends AppCompatActivity{
                 //TODO: Recycler view check... and scrolling
                // case 0: return LoginFragment.newInstance("loginFragment, Instance 1");
                 case 0: return SearchableFragment.newInstance("browseFragment, instance1");
-                case 1: return GameFragment.newInstance("gameFragment, Instance 1");
+                case 1:
+                    if (gameFragment == null) {
+                        gameFragment = GameFragment.newInstance("gameFragment, Instance 1");
+                    }
+                    return gameFragment;
                 case 2: return ProfileFragment.newInstance("profileFragment, Instance 1");
                 default: return GameFragment.newInstance("gameFragment, Instance 2");
             }
         }
-
         @Override
         public int getCount() {
             return 3;
         }
-// function for adding title to the page view
-//        @Override
-//        public CharSequence(int position){
-//            switch (position){
-//                case 0:
-//
-//            }
-//        }
+
     }
+
+
 
 }
