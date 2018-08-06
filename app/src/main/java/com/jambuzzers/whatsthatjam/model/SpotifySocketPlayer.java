fadff6b3c94899308707c59e408844cc190b6e33
Callum Pe <callumpe@fb.com>
2018-08-06 13:59:48 -0700

    added some minor scoring functionality to SocketPlayer

--- app/src/main/java/com/jambuzzers/whatsthatjam/model/SpotifySocketPlayer.java
+++ app/src/main/java/com/jambuzzers/whatsthatjam/model/SpotifySocketPlayer.java
@@ -89,7 +89,8 @@ public class SpotifySocketPlayer implements SocketPlayer {
             @Override
             public void call(Object... args) {
                 int gameId = (int) args[0];
-                listener.onInvite(gameId);
+                String creator = (String) args[1];
+                listener.onInvite(gameId,creator);
             }
         });
         mSocket.on("resume", new Emitter.Listener() {
@@ -124,6 +125,20 @@ public class SpotifySocketPlayer implements SocketPlayer {
             public void call(Object... args) {
                 String id  = (String) args[0];
                 listener.onReceiveId(id);
+            }
+        });
+        mSocket.on("result", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                String result = (String) args[0];
+                listener.onReceiveId(result);
+            }
+        });
+        mSocket.on("score", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                int score = (int) args[0];
+                listener.onScore(score);
             }
         });
     }
