e593e6eda3d68d6faed2fa7a05322a5653c4c7bd
Callum Pe <callumpe@fb.com>
2018-08-08 10:12:16 -0700

    removed action bar from games fragment and added spot for standings

--- app/src/main/java/com/jambuzzers/whatsthatjam/GameFragment.java
+++ app/src/main/java/com/jambuzzers/whatsthatjam/GameFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -39,6 +40,8 @@ public class GameFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_game, container, false);
         ButterKnife.bind(this,view);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+
         ivStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
